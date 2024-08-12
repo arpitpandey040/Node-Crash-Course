0c984d6876470b8a9e9f0faa1a82f7be9281c8ed
Arpit <ampandey58@gmail.com>
2024-08-12 22:46:14 +0530

    Completed 2nd lesson

---
+++ Lesson 2/server.js
@@ -0,0 +1,11 @@
+const http = require('http')
+
+const server = http.createServer((req, res) =>{
+    console.log(`request made`)
+    window.location.href(www.google.com)
+})
+// console.log(`I am server`)/
+
+server.listen(3000, 'localhost', ()=>{
+    console.log(`Listening for requests on post 3000`)
+})
