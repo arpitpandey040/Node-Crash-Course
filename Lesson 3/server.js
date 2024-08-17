1acbb265b1379b342d651fd43e5fae1008c816a7
arpitNT <arpit@nuraltech.com>
2024-08-17 11:33:19 +0530

    lesson 4 completed

--- Lesson 3/server.js
+++ Lesson 3/server.js
@@ -1,18 +1,57 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   // console.log(`request made`)/
   // console.log(req)/
   console.log(req.url, req.method);
 
+  // routing in Node.js
+  let path = "./views/";
+
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+    case "/about-me":
+      res.statusCode = 301;
+      res.setHeader("Location", "/about");
+      res.end();
+      break;
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+
   // set header content type
   // res.setHeader("Content-Type", "text/plain"); // to send the plain text
   // res.write("hello World!");
   // to send html
-  res.setHeader("Content-type", "text/html");
-  res.write("<p>Hello World</p>");
-  res.write("<p>hello again, World</p>");
-  res.end();
+  // res.setHeader("Content-type", "text/html");
+  // res.write("<p>Hello World</p>");
+  // res.write("<p>hello again, World</p>");
+  // res.end();
+
+  // return html pages
+
+  // fs.readFile("./views/index.html", (err, data) => { // instead of hard code now we can use path
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      // res.write(data);
+      // res.end();
+
+      // or we can do this like the below-
+      res.end(data);
+    }
+  });
 });
 // console.log(`I am server`)
 
