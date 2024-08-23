dd23b8a0703f2205361bd762f1527930a7988033
Arpit <ampandey58@gmail.com>
2024-08-23 08:43:15 +0530

    lesson 6 completed

--- Lesson 3 - 5/server.js
+++ Lesson 3 - 5/server.js
@@ -1,9 +1,83 @@
+// const http = require("http");
+// const fs = require("fs");
+// const _ = require("lodash");
+
+// const server = http.createServer((req, res) => {
+//   // console.log(`request made`)/
+//   // console.log(req)/
+//   // console.log(req.url, req.method);
+//   // lodash
+//   const num = _.random(0, 20);
+//   console.log(num);
+//   // this method helps to call the function only once no matter how many times i call it.
+//   const greet = _.once(() => {
+//     console.log(`Hello from lodash`);
+//   });
+//   greet();
+//   greet();
+//   greet();
+//   greet();
+//   greet();
+//   // routing in Node.js
+//   let path = "./views/";
+
+//   switch (req.url) {
+//     case "/":
+//       path += "index.html";
+//       res.statusCode = 200;
+//       break;
+//     case "/about":
+//       path += "about.html";
+//       res.statusCode = 200;
+//       break;
+//     case "/about-me":
+//       res.statusCode = 301;
+//       res.setHeader("Location", "/about");
+//       res.end();
+//       break;
+//     default:
+//       path += "404.html";
+//       res.statusCode = 404;
+//       break;
+//   }
+
+//   // set header content type
+//   // res.setHeader("Content-Type", "text/plain"); // to send the plain text
+//   // res.write("hello World!");
+//   // to send html
+//   // res.setHeader("Content-type", "text/html");
+//   // res.write("<p>Hello World</p>");
+//   // res.write("<p>hello again, World</p>");
+//   // res.end();
+
+//   // return html pages
+
+//   // fs.readFile("./views/index.html", (err, data) => { // instead of hard code now we can use path
+//   fs.readFile(path, (err, data) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       // res.write(data);
+//       // res.end();
+
+//       // or we can do this like the below-
+//       res.end(data);
+//     }
+//   });
+// });
+// // console.log(`I am server`)
+
+// server.listen(3000, "localhost", () => {
+//   console.log(`Listening for requests on post 3000`);
+// });
+
+
 const http = require("http");
 const fs = require("fs");
 const _ = require("lodash");
 
 const server = http.createServer((req, res) => {
-  // console.log(`request made`)/
+  // console.log(request made)/
   // console.log(req)/
   // console.log(req.url, req.method);
   // lodash
@@ -11,7 +85,7 @@ const server = http.createServer((req, res) => {
   console.log(num);
   // this method helps to call the function only once no matter how many times i call it.
   const greet = _.once(() => {
-    console.log(`Hello from lodash`);
+    console.log(Hello from lodash);
   });
   greet();
   greet();
@@ -65,8 +139,8 @@ const server = http.createServer((req, res) => {
     }
   });
 });
-// console.log(`I am server`)
+// console.log(I am server)
 
 server.listen(3000, "localhost", () => {
-  console.log(`Listening for requests on post 3000`);
+  console.log(Listening for requests on post 3000);
 });
---
+++ Lesson 6/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+
+// creating Express app
+const app = express();
+
+// listen for requests
+app.listen(3000);
+
+// how to stop browser caching data
+// app.use((req, res, next) => {
+//     res.setHeader('Cache-Control', 'no-store');
+//     next();
+//   });
+app.get("/", (req, res) => {
+  // res.send("<p>Hello from Express</p>");
+  res.sendFile("./views/index.html", { root: __dirname });
+});
+
+app.get("/about", (req, res) => {
+  // res.send( '<p>About Page</p>');
+  res.sendFile("./views/about.html", { root: __dirname });
+});
+
+// redirects
+
+app.get("/about-us", (req, res) => {
+  res.redirect("/about");
+});
+// 404 Page
+
+// this should be at the very end otherwise routing pages with manually redirect to 404.
+app.use((req, res) => {
+  res.sendFile("./views/404.html", { root: __dirname });
+}); // use is a middleware
